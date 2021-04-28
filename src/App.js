0d074b2a2109381b8dbe3650d12e22d664759784
tbsdvl <tristonburns97@gmail.com>
2021-04-27 17:51:48 -0700

    Adding Nav & Hero components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
