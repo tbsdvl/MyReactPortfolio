7864b1048f37d7e5a1f871caff1d4994f0907066
tbsdvl <tristonburns97@gmail.com>
2021-04-28 23:24:23 -0700

    Updating intro logic

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,8 +7,10 @@ class Intro extends Component {
             <section className="intro">
                 <img src="./images/seattle.jpg" alt="seattle"/>
                 <div className="introinfo">
-                <h2>Description</h2>
-                <p>lorem ipsum</p>
+                <h2>About Me</h2>
+                <p>I am an aspiring web developer with a background in studying Data Science from Arizona
+                    State University. I develop applications using crucial technologies such as React, express, and node.js. In my free time, I like to build miniatures, enjoy the outdoors, and code.
+                </p>
                 </div>
             </section>
         )
