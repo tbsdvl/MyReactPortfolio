cc1a7de25ce52eb75b34d559d554ff06d55ef806
tbsdvl <tristonburns97@gmail.com>
2021-05-28 19:11:08 -0700

    Updating resume link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ class Nav extends Component {
                     <li><a href="#portfolio">Portfolio</a></li>
                     <li><a href="https://github.com/tbsdvl" rel="noreferrer" target="_blank">Github</a></li>
                     <li><a href="https://www.linkedin.com/in/triston-burns-324499209/" rel="noreferrer" target="_blank">LinkedIn</a></li>
-                    <li><a href="https://github.com/tbsdvl/Portfolio/blob/main/resume.pdf" rel="noreferrer" target="_blank">Resume</a></li>
+                    <li><a href="https://github.com/tbsdvl/MyReactPortfolio/blob/main/resume.pdf" rel="noreferrer" target="_blank">Resume</a></li>
                     <li><a href="#contact">Contact Me</a></li>
                 </ul>
             </nav>
